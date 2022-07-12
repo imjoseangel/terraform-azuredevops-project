226268875f538ecfe559fc67d087d432f728400e
imjoseangel <josea.munoz@gmail.com>
2022-07-12 20:22:54 +0200

    add main

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
 #-------------------------------
 # Local Declarations
 #-------------------------------
-locals {
+resource "azuredevops_project" "main" {
+  name               = var.name
+  description        = var.description
+  visibility         = var.visibility
+  version_control    = var.version_control
+  work_item_template = var.work_item_template
+
+  features = {
+    boards       = var.features.boards
+    repositories = var.features.repositories
+    pipelines    = var.features.pipelines
+    testplans    = var.features.testplans
+    artifacts    = var.features.artifacts
+  }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,38 @@ variable "name" {
   description = "Name of the Azure DevOps project"
   type        = string
 }
+
+variable "description" {
+  description = "Description of the Azure DevOps project"
+  default     = "Managed by Terraform"
+  type        = string
+}
+
+variable "visibility" {
+  description = "Visibility of the Azure DevOps project"
+  default     = "Private"
+  type        = string
+}
+
+variable "version_control" {
+  description = "Version control of the Azure DevOps project"
+  default     = "Git"
+  type        = string
+}
+
+variable "work_item_template" {
+  description = "Work item template of the Azure DevOps project"
+  default     = "Agile"
+  type        = string
+}
+
+variable "features" {
+  type = map(string)
+  default = {
+    "boards"       = true
+    "repositories" = true
+    "pipelines"    = true
+    "testplans"    = false
+    "artifacts"    = false
+  }
+}
