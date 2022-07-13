c7d082a733aebcba26b514e06b945257c37b499c
imjoseangel <josea.munoz@gmail.com>
2022-07-13 15:13:43 +0200

    add sp

--- main.tf
+++ main.tf
@@ -16,3 +16,12 @@ resource "azuredevops_project" "main" {
     artifacts    = var.features.artifacts
   }
 }
+
+resource "azuredevops_serviceendpoint_azurerm" "mainazurerm" {
+  project_id                = azuredevops_project.main.id
+  service_endpoint_name     = var.service_endpoint_name
+  description               = var.description
+  azurerm_spn_tenantid      = var.tenantid
+  azurerm_subscription_id   = var.subscription_id
+  azurerm_subscription_name = var.subscription_name
+}
