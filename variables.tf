f588213c485903228d6675a945cd5f7aa598e0d0
imjoseangel <josea.munoz@gmail.com>
2022-07-13 20:12:25 +0200

    add subscription

--- variables.tf
+++ variables.tf
@@ -27,8 +27,36 @@ variable "work_item_template" {
   type        = string
 }
 
+variable "service_endpoint_name" {
+  description = "Name of the Azure DevOps service endpoint"
+  default     = "Azure DevOps"
+  type        = string
+}
+
+variable "description" {
+  description = "Description of the Azure DevOps service endpoint"
+  default     = "Managed by Terraform"
+  type        = string
+}
+
+variable "tenantid" {
+  description = "Tenant ID of the Azure DevOps service endpoint"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Subscription ID of the Azure DevOps service endpoint"
+  type        = string
+}
+
+variable "subscription_name" {
+  description = "Subscription name of the Azure DevOps service endpoint"
+  type        = string
+}
+
 variable "features" {
-  type = map(string)
+  type        = map(string)
+  description = "Features of the Azure DevOps project"
   default = {
     "boards"       = "enabled"
     "repositories" = "enabled"
